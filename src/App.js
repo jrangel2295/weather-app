f9e884dda59f6c18b7e387ed8929f16c0f1486c5
Johnny Rangel <jrangel2295@gmail.com>
2022-07-05 16:30:12 -0500

    starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './index.css';
+// import axios from 'axios'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p> this working</p>
+      <div className="container"></div>
+      <div className="top"></div>
+      <div className="location">
+        <h1>Austin</h1>
+      </div>
+      <div className="temp">
+        <h1>65°F</h1>
+      </div>
+      <div className="description"></div>
+      <div className="humidity">
+        <h1>spicy</h1>
+      </div>
+      <div className="feels">
+        <p>hot af</p>
+      </div>
+      <div className="bottom"></div>
+
     </div>
   );
 }
