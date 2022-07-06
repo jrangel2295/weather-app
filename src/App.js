ff30ccb37b5242ed3bb4e58dd434d0d847531817
Johnny Rangel <jrangel2295@gmail.com>
2022-07-06 15:46:52 -0500

    added functionality to searchbar & some styling

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
-import React, { useState } from 'react'
-import './index.css'
-import axios from 'axios'
+import React, { useState } from "react";
+import "./index.css";
+import axios from "axios";
 
 function App() {
-	const [data, setData] = useState({})
-	const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=2c212c1c3d954bc552ac091e646cf2f9`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2c212c1c3d954bc552ac091e646cf2f9`;
 
-	const searchLocation = event => {
-		if (event.key === 'Enter') {
-			axios.get(url).then(response => {
-				setData(response.data)
-				console.log(response.data)
-			})
-      setLocation('')
-		}
-	}
+  const searchLocation = event => {
+    if (event.key === "Enter") {
+      axios.get(url).then(response => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
 
-	return (
-		<div className="app">
-			<div className="search">
-				<input
-					value={location}
-					onChange={event => setLocation(event.target.value)}
-					placeholder="Enter Location"
-					onKeyPress={searchLocation}
-					type="text"
-				></input>
-			</div>
-			<div className="container">
-				<div className="top"></div>
-				<div className="location">
-					<h1>Austin</h1>
-				</div>
-				<div className="temp">
-					<h1>65°F</h1>
-				</div>
-				<div className="description">
-					<p>Cloudy</p>
-				</div>
-			</div>
-			<div className="bottom">
-				<div className="humidity">
-					<p>spicy</p>
-				</div>
-				<div className="feels">
-					<p>100°F</p>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          placeholder="Enter Location"
+          onKeyPress={searchLocation}
+          type="text"
+        ></input>
+      </div>
+      <div className="container">
+        <div className="top"></div>
+        <div className="location">
+          <h1>{data.name}</h1>
+        </div>
+        <div className="temp">
+          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+        </div>
+        <div className="description">
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="humidity">
+          {data.main ? <p>{data.main.humidity}%</p> : null}
+        </div>
+        <div className="feels">
+          {data.main ? <p>{data.main.feels_like.toFixed()}°F</p> : null}
+          {/* <p>feels like</p> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
